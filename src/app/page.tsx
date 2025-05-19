be6e9b330325637449ce7807afe1ef4a535abdd3
Samaan Sayed <nasifasayed@gmail.com>
2025-05-19 16:52:29 +0530

    added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import Image from "next/image";
 import WishForm from "@/components/WishForm";
 import WishDisplay from "@/components/WishDisplay";
 import { generateWish, WishInputs } from "@/utils/wishService";
