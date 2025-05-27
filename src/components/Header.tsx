bca3fadc03412b7d83ed62983d68d34909b25acd
Samaan Sayed <nasifasayed@gmail.com>
2025-05-27 17:53:25 +0530

    prev works

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -97,9 +97,10 @@ const Header: React.FC<HeaderProps> = ({ onLogin, onBuyCredits }) => {
                     fillRule="evenodd"
                     d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
                     clipRule="evenodd"
-                  />
+                  />{" "}
                 </svg>
-              </button>{" "}
+              </button>
+
               {isDropdownOpen && (
                 <div className="absolute right-0 mt-2 w-56 bg-white dark:bg-gray-800 rounded-xl shadow-xl border border-gray-200 dark:border-gray-700 py-2 z-50 animate-slide-in">
                   {/* User Info Section */}
