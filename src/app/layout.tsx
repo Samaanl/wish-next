256a31658f42acd56c2adbb4904ebb742de58e59
Samaan Sayed <nasifasayed@gmail.com>
2025-06-03 11:04:09 +0530

    added umami

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import ClientAuthProvider from "./providers";
 
@@ -31,6 +32,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Umami Analytics */}
+        <Script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="c75de7a2-7788-4155-bcf0-3d1fba216967"
+          strategy="afterInteractive"
+        />
         <ClientAuthProvider>{children}</ClientAuthProvider>
       </body>
     </html>
